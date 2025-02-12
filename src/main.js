1b591a32b410d68668a50609f0d7831c52045738
Siddhant Singh Karki <siddhantskarki@gmail.com>
2025-02-12 13:03:40 +1100

    -

--- src/main.js
+++ src/main.js
@@ -16,13 +16,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Firebase configuration and initialization
 const firebaseConfig = {
-  apiKey: "AIzaSyByP2FBVaQbhbcygnwrUuSmTwt9RBlvIhE",
-  authDomain: "rtc-video-chat-29a80.firebaseapp.com",
-  projectId: "rtc-video-chat-29a80",
-  storageBucket: "rtc-video-chat-29a80.firebasestorage.app",
-  messagingSenderId: "128189272045",
-  appId: "1:128189272045:web:dcb48a8c7344436ff3a1e4",
-  measurementId: "G-T87JHWLQL6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
